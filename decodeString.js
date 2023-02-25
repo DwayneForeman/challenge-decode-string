14d31da2727599af56cc9bfb69c8b05a95b6f000
Dwayne Foreman <dwayneforemanjr@yahoo.com>
2023-02-25 11:23:58 -0500

    second commit

--- decodeString.js
+++ decodeString.js
@@ -13,3 +13,16 @@ The number in the string represents how many characters each letter should shift
 >>> "2fcjjm" // "hello"
 
 */
+
+function decoder(s) {
+  const shift = parseInt(s[0]); // extract the shift value from the first character of the input string
+  let result = ""; // initialize an empty string to store the decoded result
+
+  for (let i = 1; i < s.length; i++) {
+    const charCode = s.charCodeAt(i); // get the character code of the current character
+    const shiftedCode = ((charCode - shift - 97 + 26) % 26) + 97; // shift the character code and wrap around if necessary
+    result += String.fromCharCode(shiftedCode); // convert the shifted character code to its corresponding character and append to the result string
+  }
+
+  return result;
+}
